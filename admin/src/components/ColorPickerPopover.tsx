a07189d8c05c7ce97b1bc395011c33a754744f00
Dominik Juriga <dominik.juriga@notum.cz>
2026-03-17 11:44:56 +0100

    chore: move remove color button to the top

--- admin/src/components/ColorPickerPopover.tsx
+++ admin/src/components/ColorPickerPopover.tsx
@@ -19,6 +19,17 @@ export function ColorPickerPopover({
 
   return (
     <div style={{ padding: 8, maxHeight: 400, overflowY: 'auto' }}>
+      <Button
+        variant="tertiary"
+        size="S"
+        onClick={onRemove}
+        style={{ marginTop: 8, width: '100%' }}
+      >
+        {formatMessage({
+          id: 'tiptap-editor.color.remove',
+          defaultMessage: 'Remove color',
+        })}
+      </Button>
       <div
         style={{
           display: 'grid',
@@ -48,17 +59,6 @@ export function ColorPickerPopover({
           </Tooltip>
         ))}
       </div>
-      <Button
-        variant="tertiary"
-        size="S"
-        onClick={onRemove}
-        style={{ marginTop: 8, width: '100%' }}
-      >
-        {formatMessage({
-          id: 'tiptap-editor.color.remove',
-          defaultMessage: 'Remove color',
-        })}
-      </Button>
     </div>
   );
 }
